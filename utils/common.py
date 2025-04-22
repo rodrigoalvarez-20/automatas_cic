34530b4eb0742ea79f299f62de5e8ae43cd2db1f
Rodrigo Alvarez <rodrigoalvarez449@gmail.com>
2025-04-22 10:52:42 -0600

    Se ha agregado el metodo de Qrip para RE

--- utils/common.py
+++ utils/common.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+
 def parse_arguments():
     if len(sys.argv) != 3:
         raise Exception("Instrucciones de ejecucion:\npython automaton.py <ruta_archivo_definicion> <cadena_a_evaluar>")
@@ -31,30 +32,3 @@ def load_automata_definition(definition_file: str):
         state_trans_dict = { v: state_transitions[idx] for idx, v in enumerate(vocab) }
         states_definition_dict[state_id] = state_trans_dict
     return vocab, accept_states, states_definition_dict
-
-
-def test_string(input_data: str, states_definition: dict, vocab: list):
-    eval_items = [ c for c in input_data.strip() ]
-    bad_items = list(set(eval_items) - set(vocab))
-    if len(bad_items) > 0:
-        raise Exception("La cadena introducida contiene caracteres fuera del vocabulario: {}".format(bad_items))
-    actual_state = list(states_definition.keys())[0] #"1"
-    for item in eval_items:
-        state_trans = states_definition.get(actual_state)
-        if not state_trans:
-            raise Exception("No se ha encontrado la definicion del estado '{}' en la tabla de transiciones.".format(actual_state))
-        next_state = state_trans.get(item)
-        if not next_state:
-            raise Exception("La transicion del estado '{}' usando '{}' es invalida".format(actual_state, item))
-        actual_state = next_state
-    return actual_state
-    
-
-def main():
-    def_file, input_data = parse_arguments()
-    vocab, accepted_states, states_definition = load_automata_definition(def_file)
-    final_state = test_string(input_data, states_definition, vocab)
-    print("La cadena '{}' es {}".format(input_data, "aceptada" if final_state in accepted_states else "invalida"))
-
-if __name__ == "__main__":
-    main()
